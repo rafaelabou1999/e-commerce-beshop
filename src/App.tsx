ca837cc198fbaa5f5412a4968ed5d7eb0c80a4cf
Rafaela Bourdette <rafaelabourdette@gmail.com>
2025-07-04 08:14:14 -0300

    deleting sidenav

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './root/styles.css';
 import { ProductProvider } from './context/ProductProvider';
 import { CompProduct } from './components/CompProduct/CompProduct';
 import { ToastContainer } from 'react-toastify';
-import { CompSideNav } from './components/CompSideNav';
 import { CompHeader } from './components/CompHeader';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { CartPage } from './pages/CartPage';
